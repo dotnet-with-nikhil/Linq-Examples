2282aa892b4b813f1e1992a3da002eb866c97494
dotnet-with-nikhil <crackdotnetinterviewsprep@gmail.com>
2026-08-31 23:36:58 +0530

    added small changes

--- Linq-Examples/Linq-Examples/Program.cs
+++ Linq-Examples/Linq-Examples/Program.cs
@@ -1,3 +1,12 @@
-﻿using System.Diagnostics;
+﻿using LinqPractice;
+using System.Diagnostics;
+using System.Runtime.Intrinsics.X86;
 
-Trace.WriteLine("Hello, World!");
+
+//Use First() when you want the first matching record and you're confident that at least one record exists.
+var employee = StaticData.Employees
+    .First(e => e.DepartmentId == 1);
+
+Trace.WriteLine("*******Output******\n\n\n");
+Trace.WriteLine(employee?.Name);
+Trace.WriteLine("\n\n\n");
